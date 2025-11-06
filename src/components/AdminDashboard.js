c18fc2c94098c6d13ac2de796a086f11881e5b06
Arvindkumawat341 <arvind.kumawat@maitriiinfotech.com>
2025-11-06 23:18:00 +0530

    update code

--- src/components/AdminDashboard.js
+++ src/components/AdminDashboard.js
@@ -3,7 +3,6 @@ import { UserCog } from "lucide-react";
 const AdminDashboard = () => {
   return (
     <div className="min-h-screen bg-gray-100">
-      {/* Top Header */}
       <header className="bg-white shadow sticky top-0 z-10">
         <div className="max-w-7xl mx-auto mt-10 px-6 py-4 flex justify-between items-center">
           <h1 className="text-2xl font-bold text-gray-800 flex items-center gap-2">
